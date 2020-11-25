c7fba88b3cbe9d1c1db460b22f2c36c16482ff6d
Ben Fox <bfox11677@gmail.com>
2020-11-25 16:42:18 -0600

    t-shirt section completed

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -17,4 +17,32 @@ jobRoleSelect.addEventListener('change', e => {
     }
 });
 
-// (5) "T-Shirt Info" section: 
+// (5) "T-Shirt Info" section: Colors are disabled until a design is chosen then only colors available per design are enabled.
+const designSelectElement = document.querySelector('#design');
+const colorSelectElement = document.querySelector('#color');
+const colorOptionElements = colorSelectElement.children;
+
+console.log(designSelectElement);
+console.log(colorSelectElement);
+console.log(colorOptionElements);
+
+colorSelectElement.disabled = true;
+
+designSelectElement.addEventListener('change', e => {
+    colorSelectElement.disabled = false;
+    for (let i = 0; i < colorOptionElements.length; i++) {
+        const designTheme = e.target.value;
+        console.log(designTheme);
+        let colorTheme = colorOptionElements[i].getAttribute('data-theme');
+        console.log(colorTheme);
+        if(designTheme === colorTheme) {
+            console.log('equal');
+            colorOptionElements[i].hidden = false;
+            colorOptionElements[i].selected = true;
+        } else {
+            console.log('false');
+            colorOptionElements[i].hidden = true;
+            colorOptionElements[i].selected = false;
+        }
+    }
+});
