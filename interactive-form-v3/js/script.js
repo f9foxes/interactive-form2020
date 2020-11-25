26d107516a14f0a1c03534856658323b09be594a
Ben Fox <bfox11677@gmail.com>
2020-11-25 14:56:49 -0600

    job role section upddated

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,20 @@
-//Focus inside name input on page load.
+// (3) The "Name" field: Focus inside name input on page load. 
 const inputName = document.getElementById('name');
 inputName.focus();
 
+// (4) "Job Role" section: Toggle other job role input display to block if other job role is selected
+const jobRoleSelect = document.getElementById('title');
+const otherJobRoleInput = document.getElementById('other-job-role');
+
+otherJobRoleInput.style.display = 'none';
+
+jobRoleSelect.addEventListener('change', e => {
+    if (e.target.value === 'other') {
+        otherJobRoleInput.style.display = 'block';
+    }
+    else {
+        otherJobRoleInput.style.display = 'none';
+    }
+});
+
+// (5) "T-Shirt Info" section: 
